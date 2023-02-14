f2109b0943898f998810584c5454153e148ac914
Tyler Ting <juting@students.pitzer.edu>
2023-02-13 19:41:01 -0800

    fixed word_ladder

--- word_ladder.py
+++ word_ladder.py
@@ -50,7 +50,6 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     return None
 
 
-
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
